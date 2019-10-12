a001e44ceac05b78262d742b561b8339b2327618
Fabio Romanini <fabioaromanini@gmail.com>
2019-10-12 11:31:19 -0300

    adds api call to alpha vantage for daily data retrieval

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
+const alphaVantageService = require('./services/alphaVantage');
 
 exports.dailyInfo = async (request, response) => {
-  response.status(200).send('Hello World!');
+  const data = await alphaVantageService.dailyInfo('MSFT');
+  response.status(200).send(`${Object.keys(data).length} objects retrieved from API`);
 };
---
+++ services/alphaVantage.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+const conf = require('../conf/env');
+
+const { API_KEY, BASE_URL } = conf;
+
+exports.dailyInfo = async symbol => {
+  const { data } = await axios.get(
+    BASE_URL
+    + '?function=TIME_SERIES_INTRADAY&outputsize=full'
+    + `&symbol=${symbol}&interval=1min&apikey=${API_KEY}`
+  );
+  return data;
+};
