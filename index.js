7b691df2a045cfd589e1acf210d44fba87de6c88
Fabio Romanini <fabioaromanini@gmail.com>
2019-10-15 01:52:48 -0300

    updates function trigger to pubsub subcription and creates topic on serverless.yml

--- index.js
+++ index.js
@@ -2,14 +2,11 @@
 const alphaVantageService = require('./services/alphaVantage');
 const storageService = require('./services/storage');
 
-exports.dailyInfo = async (request, response) => {
-  const symbol = 'MSFT';
+exports.dailyInfo = async (symbol) => {
   const dailyInfoData = await alphaVantageService.dailyInfo(symbol);
   const dataSize = dailyInfoData.meta.dataLenght;
   console.log(`${dataSize} objects retrieved from API`);
   
   await storageService.saveData(`${symbol}.json`, 'api-responses', dailyInfoData);
   console.log(`${dataSize} stored`);
-
-  response.status(200).send(JSON.stringify(dataSize));
 };
