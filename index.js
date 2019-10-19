79880c0584aea8cd346aa1be702e7df878e5602d
Fabio Romanini <fabioaromanini@gmail.com>
2019-10-19 12:14:28 -0300

    adds pubsub system for triggering dailyInfo from minuteLoader

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 'use strict'
 const alphaVantageService = require('./services/alphaVantage');
+const pubsubService = require('./services/pubsub');
 const storageService = require('./services/storage');
 
-exports.dailyInfo = async (symbol) => {
+exports.dailyInfo = async (event) => {
+  const symbol = pubsubService.parseMessage(event);
   const dailyInfoData = await alphaVantageService.dailyInfo(symbol);
   const dataSize = dailyInfoData.meta.dataLenght;
-  console.log(`${dataSize} objects retrieved from API`);
+  console.log(`${dataSize} objects retrieved from API for ${symbol}`);
   
   await storageService.saveData(`${symbol}.json`, 'api-responses', dailyInfoData);
-  console.log(`${dataSize} stored`);
+  console.log(`${dataSize} stored for ${symbol}`);
 };
 
+const { DAILY_INFO_TOPIC } = process.env;
+
 exports.minuteLoader = async () => {
-  // get files in current date bucket path
-  // verify which are not yet download
-  // publish 4 messages on pubsub accordingly
+  // 1 - get files in current date bucket path
+  // 2 - verify which are not yet download
+  // 3 - select 4
+  await pubsubService.publishMessage('MSFT', DAILY_INFO_TOPIC);
 };
