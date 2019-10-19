3ba016a731656077993b4ab2cef02e728a089e1e
Fabio Romanini <fabioaromanini@gmail.com>
2019-10-19 02:30:32 -0300

    adds pubsub message parsing

--- services/pubsub.js
+++ services/pubsub.js
@@ -1,5 +1,6 @@
 // Imports the Google Cloud client library
 const { PubSub } = require('@google-cloud/pubsub');
+const atob = require('atob');
 
 const client = new PubSub();
 
@@ -7,3 +8,5 @@ exports.publishMessage = async (message, topic) => {
   const dataBuffer = Buffer.from(message);
   return client.topic(topic).publish(dataBuffer);
 }
+
+exports.parseMessage = message => atob(message.data);
