3440811680ba0c6d385301c354b56e13f1342889
Fabio Romanini <fabioaromanini@gmail.com>
2019-10-24 00:32:22 -0300

    writes filtered and parsed data to persistent data lake in a new line delimited json

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ exports.dailyInfo = async (event) => {
   const dataSize = dailyInfoData.meta.dataLenght;
   console.log(`${dataSize} objects retrieved from API for ${symbol}`);
 
-  await storageService.saveData(`${symbol}.json`, 'api-responses', dailyInfoData);
+  await storageService.saveJsonData(`${symbol}.json`, 'api-responses', dailyInfoData);
   console.log(`${dataSize} stored for ${symbol}`);
 };
 
@@ -29,12 +29,15 @@ exports.apiResponseFilter = async apiResponseFile => {
 
   const { data, meta } = JSON.parse(fileContent);
   const dateToInclude = meta["3. Last Refreshed"].split(' ')[0]; // format = 2019-10-14 16:00:00
-  
-  const dailyEvents = (
-    Object
-      .keys(data)
-      .filter(key => key.includes(dateToInclude))
-      .map(key => dataParsingService.parseMinuteEvent(key, data, meta))
-  );
-  console.log(`Got ${dailyEvents.lenght} events for ${dateToInclude} in file ${apiResponseFile.name}`);
+
+  const dailyEvents = Object
+    .keys(data)
+    .filter(key => key.includes(dateToInclude))
+    .map(key => dataParsingService.parseMinuteEvent(key, data, meta));
+
+  console.log(`Got ${dailyEvents.length} events for ${dateToInclude} in file ${apiResponseFile.name}`);
+
+  const newFileName = `${apiResponseFile.name}l`;
+  await storageService.saveJsonlData(newFileName, 'persistent-parsed-data', dailyEvents);
+  console.log(`${dailyEvents.length} stored for ${newFileName}`);
 };
--- services/storage.js
+++ services/storage.js
@@ -2,13 +2,26 @@ const { Storage } = require('@google-cloud/storage');
 
 const client = new Storage();
 
-exports.saveData = async (filename, bucket, content) => {
+exports.saveJsonData = async (filename, bucket, content) => {
   const fullFilename = `${content.meta.date}/${filename}`;
-  file = client.bucket(bucket).file(fullFilename);
+  const file = client.bucket(bucket).file(fullFilename);
   const parsedContent = JSON.stringify(content);
   await file.save(
-    parsedContent, 
+    parsedContent,
     { metadata: { contentType: 'application/json' } }
+  );
+};
+
+exports.saveJsonlData = async (filename, bucket, content) => {
+  const file = client.bucket(bucket).file(filename);
+
+  const parsedContent = content
+    .map(element => JSON.stringify(element))
+    .reduce((prev, curr) => `${prev}${curr}\n`, '');
+
+  await file.save(
+    parsedContent,
+    { metadata: { contentType: 'application/x-ndjson' } }
   );
 };
 
