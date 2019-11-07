ea4045d4cabeb154477af7a829f9ab7e281bcca5
Fabio Romanini <fabioaromanini@gmail.com>
2019-11-07 15:56:12 -0300

    adds greatestVariation to query creator

--- src/services/query.js
+++ src/services/query.js
@@ -1,7 +1,8 @@
 const queries = {
   minMax: require('../queries/minMax'),
   repeatedStockData: require('../queries/repeatedStockData'),
-  deduplication: require('../queries/deduplication')
+  deduplication: require('../queries/deduplication'),
+  greatestVariation: require('../queries/greatestVariation')
 }
 
 exports.getQueryCreator = name => queries[name];
